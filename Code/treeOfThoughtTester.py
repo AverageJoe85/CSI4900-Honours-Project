e802a0707683a643f556700cdb7cedd667204b22
AverageJoe85 <43150681+AverageJoe85@users.noreply.github.com>
2025-04-13 19:01:19 -0400

    Automated tester for 100 games

--- Code/treeOfThoughtTester.py
+++ Code/treeOfThoughtTester.py
@@ -1,16 +1,17 @@
 import gameOf24ToT
-
-gameOf24ToT.numbers = [1, 1, 4, 6]
-
+import examples
 
 solves = 0
 fails = 0
-if gameOf24ToT.run():
-    solves += 1
-    print("Solves +1")
-else:
-    fails += 1
-    print("Fails +1")
+
+for numbers in examples.examples:
+    gameOf24ToT.numbers = numbers
+    if gameOf24ToT.run():
+        solves += 1
+        print("Solves +1")
+    else:
+        fails += 1
+        print("Fails +1")
 
 solvePercent = solves / (solves + fails)
-print(f"\nSolve Percentage = {(solvePercent * 100).2f}%")
+print(f"\nSolve Percentage = {(solvePercent * 100):.2f}%")
