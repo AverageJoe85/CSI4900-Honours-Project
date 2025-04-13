12dbefe0fb5002827083864c0e554fb7351e3e9e
AverageJoe85 <43150681+AverageJoe85@users.noreply.github.com>
2025-04-13 17:48:35 -0400

    First iteration of automated testing

--- Code/treeOfThoughtTester.py
+++ Code/treeOfThoughtTester.py
@@ -2,4 +2,15 @@ import gameOf24Optimized
 
 gameOf24Optimized.numbers = [1, 1, 4, 6]
 
-gameOf24Optimized.run()
+
+solves = 0
+fails = 0
+if gameOf24Optimized.run():
+    solves += 1
+    print("Solves +1")
+else:
+    fails += 1
+    print("Fails +1")
+
+solvePercent = solves / (solves + fails)
+print(f"Solve Percentage = %{solvePercent * 100}")
